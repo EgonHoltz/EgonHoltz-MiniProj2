169d145027154dbb0152ff45f80afc6f2cbfeb9f
Egon Holtz <egonholtz@gmail.com>
2021-12-05 23:55:59 +0000

    Deleted HelloWorld to run app, started to implement Login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <b-container fluid>
-
+    <!--Header -->
+    <!--Rules (how to play?) -->
+    <!--Contacts -->
+    <!--Footer -->
+    <!--Copyright -->
   </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+
   },
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+    name: "Login",
+    components: {},
+    data: function() {
+        return {    
+            username: "",
+            password: ""
+        };
+    },
+    methods: {
+        login() {
+        this.$store
+            .dispatch(`${MODULE_BASE}/${AUTH_LOGIN}`, this.$data)
+            .then(() => {
+                this.$alert(this.getMessage, "Login", "success");
+                router.push({ name: "home" });
+            })
+            .catch(err => {
+            this.$alert(`${err.message}`, "Erro", "error");
+            });
+        }
+    }
+};
+</script>
