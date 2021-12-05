18fbff67d14519c5c57d007bcf16f4777e67834e
Egon Holtz <egonholtz@gmail.com>
2021-12-05 21:25:26 +0000

    creating modules of app

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+//Modules imports
+import moduleBase from "./module";
+import moduleAuth from "./auth/auth.module";
+import moduleUser from "./user/user.module";
+import moduleAnimal from "./animals/animal.module";
+import moduleQuizz from "./quizzes/quizz.module";
+import moduleQuestion from "./questions/question.module";
+
 Vue.use(Vuex);
 
+//Store instanciation
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  getters: moduleBase.getters,
+  state: moduleBase.state,
+  mutations: moduleBase.mutations,
+  actions: moduleBase.actions,
+  modules: {
+    auth: moduleAuth,
+    user: moduleUser,
+    animal: moduleAnimal,
+    quizz: moduleQuizz,
+    question: moduleQuestion
+  }
 });
