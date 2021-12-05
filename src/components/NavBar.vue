b0d530104b56c7fbdc32b4ee4adaa898c8cb7caa
Egon Holtz <egonholtz@gmail.com>
2021-12-05 23:00:08 +0000

    Adding NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <b-nav-item :to="{name: 'animals'}" :disabled=!isUserLoggedIn>
+            ANIMAIS
+        </b-nav-item>
+        <b-nav-item :to="{name: 'quizzes'}" :disabled=!isUserLoggedIn>
+            QUIZZES
+        </b-nav-item>
+        <b-nav-item :to="{name: 'contactos'}">
+            CONTACTOS
+        </b-nav-item>
+        <b-nav-item :to="{name: 'login'}">
+            LOGIN
+        </b-nav-item>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters("auth", [
+      "isUserLoggedIn",
+      "getProfileName",
+      "getUserType",
+      "getProfile"
+    ])
+  }
+};
+</script>
