2ddf7b13c6b003bf11cf5e622b116b0eb2139441
Egon Holtz <egonholtz@gmail.com>
2021-12-05 21:58:19 +0000

    Added libaries to projet

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVue from "bootstrap-vue"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSimpleAlert from "vue-simple-alert"
+
+Vue.use(BootstrapVue)
+Vue.use(VueSimpleAlert)
 
 Vue.config.productionTip = false;
 
